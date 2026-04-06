893e75a1dab74d031aa77a130d3d8f3b9da30137
EdgardoCotero <144290878+EdgardoCotero@users.noreply.github.com>
2026-04-06 15:01:43 -0600

    12

--- hablarapp/backend/src/routes/pacientes.routes.js
+++ hablarapp/backend/src/routes/pacientes.routes.js
@@ -10,13 +10,8 @@ const {
 } = require("../controllers/pacientes.controller");
 
 // --- RUTAS ---
-
-// 1. GET / - QUITAMOS 'auth' y 'requireTerapeuta' para que Android pueda entrar sin Token
-router.get("/", getPacientes); 
-
-// 2. Las demás rutas las dejamos protegidas (por ahora no las usaremos en el Home)
+router.get("/", auth, requireTerapeuta, getPacientes);
 router.get("/:id", auth, allowTerapeutaOrSelfPaciente, getPacienteById);
 router.put("/:id", auth, allowTerapeutaOrSelfPaciente, updatePaciente);
 router.delete("/:id", auth, requireTerapeuta, deletePaciente);
-
 module.exports = router;

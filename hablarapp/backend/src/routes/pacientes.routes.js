2ad9cbfd5022c4d671d20849cac1a9e4ca976499
EdgardoCotero <144290878+EdgardoCotero@users.noreply.github.com>
2026-04-16 10:04:27 -0600

    30

--- hablarapp/backend/src/routes/pacientes.routes.js
+++ hablarapp/backend/src/routes/pacientes.routes.js
@@ -1,39 +1,33 @@
 const router = require("express").Router();
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const { auth, requireTerapeuta, allowTerapeutaOrSelfPaciente } = require("../middlewares/auth");
-const { 
-    getPacientes, getPacienteById, updatePaciente, deletePaciente,
-    guardarProgreso, getProgresoDia, getEjerciciosAsignados,
-    subirOrofacial, subirFonetico,
-    getHistorialMensual 
+const {
+  auth,
+  requireTerapeuta,
+  allowTerapeutaOrSelfPaciente,
+} = require("../middlewares/auth");
+
+const {
+  getPacientes,
+  getPacienteById,
+  updatePaciente,
+  deletePaciente,
+  guardarProgreso,
+  getProgresoDia,
+  getEjerciciosAsignados,
+  getHistorialMensual,
 } = require("../controllers/pacientes.controller");
-
-// Crear carpetas si no existen
-const uploadDir = path.join(__dirname, '../../uploads');
-if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
-if (!fs.existsSync(path.join(uploadDir, 'orofaciales'))) fs.mkdirSync(path.join(uploadDir, 'orofaciales'));
-if (!fs.existsSync(path.join(uploadDir, 'foneticos'))) fs.mkdirSync(path.join(uploadDir, 'foneticos'));
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const type = file.mimetype.startsWith('image/') ? 'orofaciales' : 'foneticos';
-        cb(null, path.join(__dirname, '../../uploads', type));
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-});
-const upload = multer({ storage });
 
 // --- RUTAS DE PACIENTES ---
 router.get("/", auth, requireTerapeuta, getPacientes);
 router.post("/guardar-progreso", auth, guardarProgreso);
+
 router.get("/:id/asignados", auth, getEjerciciosAsignados);
 router.get("/:id/progreso-dia", auth, getProgresoDia);
-
-router.get("/progreso/historial/:id/:mes/:anio", auth, allowTerapeutaOrSelfPaciente, getHistorialMensual);
+router.get(
+  "/progreso/historial/:id/:mes/:anio",
+  auth,
+  allowTerapeutaOrSelfPaciente,
+  getHistorialMensual
+);
 
 router.get("/:id", auth, allowTerapeutaOrSelfPaciente, getPacienteById);
 router.put("/:id", auth, allowTerapeutaOrSelfPaciente, updatePaciente);

1d100d94c10034fd5f29a414002ba30ec257a342
EdgardoCotero <144290878+EdgardoCotero@users.noreply.github.com>
2026-05-04 18:19:40 -0600

    c44

--- hablarapp/backend/src/db.js
+++ hablarapp/backend/src/db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2/promise");
 
 
 
-const DB_HOST = "192.168.0.175";
+const DB_HOST = "hablar-d.cojbkorabely.us-east-1.rds.amazonaws.com";
 const DB_PORT = 3306;
-const DB_USER = "root";
+const DB_USER = "admin";
 const DB_PASS = "maria123?";
 const DB_NAME = "app_tshusuarios";
 
